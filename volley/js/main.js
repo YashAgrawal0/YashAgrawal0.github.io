018cf3ef1d8b2c30f5aeac86e413b57f102da519
YashAgrawal0 <yashagrawal048@gmail.com>
2020-05-16 15:12:51 +0530

    Webpage1.0

--- volley/js/main.js
+++ volley/js/main.js
@@ -9,8 +9,8 @@ jQuery(document).ready(function($) {
 	"use strict";
 
 	
-	$(".loader").delay(1000).fadeOut("slow");
-  $("#overlayer").delay(1000).fadeOut("slow");	
+	$(".loader").delay(20000).fadeOut("slow");
+  $("#overlayer").delay(20000).fadeOut("slow");	
   
 
 	var siteMenuClone = function() {
