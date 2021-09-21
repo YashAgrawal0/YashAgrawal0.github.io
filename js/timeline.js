139b1a49425226619f9e29931d0d03d4066b17dc
YashAgrawal0 <b16120@students.iitmandi.ac.in>
2021-09-21 11:45:27 +0530

    Changed timeline
    Signed-off-by: YashAgrawal0 <b16120@students.iitmandi.ac.in>

--- js/timeline.js
+++ js/timeline.js
@@ -2,11 +2,21 @@ $(document).ready(function(){
     $(window).scroll(function(){
 		var hT = $('#timeline').offset().top;
 		var hS = $(this).scrollTop();
-		// console.log(hT - hS);
+		console.log(hS - hT);
 		// console.log(elementClicked);
 		project_top = $('#projects').offset().top;
 
-		if(hT - hS < 100){
+
+		if(hS - hT > -100){
+    		// $("#title_2019").fadeOut(500);
+		    $("#img_2020").fadeIn(1000);
+		}
+		else{
+		    $("#img_2020").fadeOut(500);
+    		// $("#title_2019").fadeIn(1000);
+		}
+
+		if(hS - hT > 100){
     		// $("#title_2019").fadeOut(500);
 		    $("#img_2019").fadeIn(1000);
 		}
@@ -15,7 +25,7 @@ $(document).ready(function(){
     		// $("#title_2019").fadeIn(1000);
 		}
 
-		if(hS - hT > 300){
+		if(hS - hT > 600){
     		// $("#title_2018").fadeOut(500);
 		    $("#img_2018").fadeIn(1000);
 		}
@@ -25,7 +35,7 @@ $(document).ready(function(){
 		}
 
 
-		if(hS - hT > 600){
+		if(hS - hT > 900){
     		// $("#title_2016").fadeOut(500);
 		    $("#img_2016").fadeIn(1000);
 		}
@@ -35,7 +45,7 @@ $(document).ready(function(){
 		}
 
 
-		if(hS - hT > 900){
+		if(hS - hT > 1200){
     		// $("#titsle_2015").fadeOut(500);
 		    $("#img_2015").fadeIn(1000);
 		}
@@ -45,7 +55,7 @@ $(document).ready(function(){
 		}
 
 
-		if(hS - hT > 1000){
+		if(hS - hT > 1500){
     		// $("#title_2008").fadeOut(500);
 		    $("#img_2008").fadeIn(1000);
 		}
