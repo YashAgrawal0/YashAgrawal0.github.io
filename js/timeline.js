15855afa28714f593672b3f05b07213f0cf260f7
yashagrawal0 <yashagrawal048@gmail.com>
2024-11-10 20:46:55 +0530

    timeline update

--- js/timeline.js
+++ js/timeline.js
@@ -8,54 +8,66 @@ $(document).ready(function(){
 
 
 		if(hS - hT > -100){
-    		// $("#title_2019").fadeOut(500);
+		    $("#img_2024").fadeIn(1000);
+		}
+		else{
+		    $("#img_2024").fadeOut(500);
+		}
+
+		if(hS - hT > 100){
+		    $("#img_2022").fadeIn(1000);
+		}
+		else{
+		    $("#img_2022").fadeOut(500);
+		}
+
+		if(hS - hT > 600){
 		    $("#img_2020").fadeIn(1000);
 		}
 		else{
 		    $("#img_2020").fadeOut(500);
-    		// $("#title_2019").fadeIn(1000);
-		}
-
-		if(hS - hT > 100){
-    		// $("#title_2019").fadeOut(500);
-		    $("#img_2019").fadeIn(1000);
-		}
-		else{
-		    $("#img_2019").fadeOut(500);
-    		// $("#title_2019").fadeIn(1000);
-		}
-
-		if(hS - hT > 600){
-    		// $("#title_2018").fadeOut(500);
-		    $("#img_2018").fadeIn(1000);
-		}
-		else{
-		    $("#img_2018").fadeOut(500);
-    		// $("#title_2018").fadeIn(1000);
 		}
 
 
 		if(hS - hT > 900){
     		// $("#title_2016").fadeOut(500);
-		    $("#img_2016").fadeIn(1000);
+		    $("#img_2019").fadeIn(1000);
 		}
 		else{
-		    $("#img_2016").fadeOut(500);
+		    $("#img_2019").fadeOut(500);
     		// $("#title_2016").fadeIn(1000);
 		}
 
 
 		if(hS - hT > 1200){
     		// $("#titsle_2015").fadeOut(500);
-		    $("#img_2015").fadeIn(1000);
+		    $("#img_2018").fadeIn(1000);
 		}
 		else{
-		    $("#img_2015").fadeOut(500);
+		    $("#img_2018").fadeOut(500);
     		// $("#title_2015").fadeIn(1000);
 		}
 
 
 		if(hS - hT > 1500){
+    		// $("#title_2008").fadeOut(500);
+		    $("#img_2016").fadeIn(1000);
+		}
+		else{
+		    $("#img_2016").fadeOut(500);
+    		// $("#title_2008").fadeIn(1000);
+		}
+
+		if(hS - hT > 2000){
+    		// $("#title_2008").fadeOut(500);
+		    $("#img_2015").fadeIn(1000);
+		}
+		else{
+		    $("#img_2015").fadeOut(500);
+    		// $("#title_2008").fadeIn(1000);
+		}
+
+		if(hS - hT > 2300){
     		// $("#title_2008").fadeOut(500);
 		    $("#img_2008").fadeIn(1000);
 		}
